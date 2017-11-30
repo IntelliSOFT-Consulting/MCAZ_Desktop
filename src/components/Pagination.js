d777f59aecfc7c4386afbc59ebeb77b39001afa5
John Kenny Muiruri <john.k.muiruri@gmail.com>
2017-11-30 07:33:28 +0300

    added comments

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -7,11 +7,15 @@ export default class Pagination extends Component {
     this.getPageItems = this.getPageItems.bind(this)
     this.onChange = this.onChange.bind(this)
     this.getPage = this.getPage.bind(this)
+    this.getNumberOfPages = this.getNumberOfPages.bind(this)
 
     const { activePage } = this.props
     this.state = { activePage }
   }
 
+  /**
+    Invoked when page number changes.
+  */
   onChange(pageNumber) {
     const { onChange } = this.props
     this.setState({ activePage : pageNumber })
@@ -20,10 +24,21 @@ export default class Pagination extends Component {
     }
   }
 
-  getPageItems() {
+  /**
+    Returns the number of pages for the current view.
+  */
+  getNumberOfPages() {
     const { total, itemsPerPage } = this.props
-    const { activePage } = this.state
     const noPages = Math.ceil(total / itemsPerPage)
+    return noPages
+  }
+
+  /**
+    Returns the pagination li items.
+  */
+  getPageItems() {
+    const { activePage } = this.state
+    const noPages = this.getNumberOfPages()
     var items = [], i = 0
     items.push((
       <li className="pointer" key={ Math.floor(Math.random() * 10000000) } onClick={ () => onChange(this.getPage(this.state.activePage - 1)) }>
@@ -52,6 +67,10 @@ export default class Pagination extends Component {
     return items
   }
 
+  /**
+    Get the page to be rendered.
+    When you click on next or previous
+  */
   getPage(page) {
     const { total, itemsPerPage } = this.props
     const noPages = Math.ceil(total / itemsPerPage)
@@ -64,6 +83,9 @@ export default class Pagination extends Component {
     return page
   }
 
+  /**
+    Renders the view.
+  */
   render() {
     const items = this.getPageItems()
     // no need for pagination if we have only one page.
