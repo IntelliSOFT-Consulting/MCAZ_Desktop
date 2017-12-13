58a4aeff46606c2a2624aebdc059055a594e23ca
John Kenny Muiruri <john.k.muiruri@gmail.com>
2017-12-13 10:08:50 +0300

    added age group calculation

--- src/forms/ADRForm.js
+++ src/forms/ADRForm.js
@@ -239,18 +239,20 @@ class ADRForm extends FormComponent {
       const time = moment().year(values[2]).month(values[1]).day(values[0])
       const now = moment()
       const age = now.diff(time, 'years', true);
-      const months = now.diff(time, 'days', true);
+      const days = now.diff(time, 'days', true);
       var age_group = ""
-      if(months <= 28) {
+      if(days <= 28) {
         age_group = "neonate"
-      } else if(age >= 50) {
+      } else if(age >= 70) {
         age_group = "elderly"
-      } else if(age >= 19) {
+      } else if(age >= 17) {
         age_group = "adult"
-      } else if(age >= 10) {
+      } else if(age >= 12) {
         age_group = "adolescent"
-      } else {
+      } else if(age >= 5) {
         age_group = "child"
+      } else {
+        age_group = "infant"
       }
       const { model } = this.state
       model['age_group'] = age_group
