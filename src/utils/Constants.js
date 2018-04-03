52f631c06ba77442ae305504bf39730075638f81
John Muiruri <john.k.muiruri@gmail.com>
2018-04-03 08:34:15 +0300

    Update Constants.js
    Updated server url

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -18,7 +18,7 @@ export const STATUS_DRAFT = 'STATUS_DRAFT'
 export const STATUS_COMPLETED = 'STATUS_COMPLETED'
 export const STATUS_UPLOADED = 'STATUS_UPLOADED'
 
-export const MAIN_URL = 'http://45.79.130.91/api'
+export const MAIN_URL = 'http://41.57.65.54:8080/api'
 
 export const MAIN_PAGE = 'MAIN_PAGE' // The main page
 export const ADR_FORM_PAGE = 'ADR_FORM_PAGE'
